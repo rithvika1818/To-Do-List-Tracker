ce55c6fd437d22ede200d0c668216bd4a73d4025
rithvika1818 <rithvika1817@gmail.com>
2024-07-25 11:49:52 +0530

    Update: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   base: '/repository-name/',  // Replace with your repository name
   plugins: [react()],
+  base: "/To-Do-List-Tracker/"
 });
